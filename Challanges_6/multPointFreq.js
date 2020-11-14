2f02a006379b8dc6e994e02926ba081bc66f9b74
Ausar <ausarmcgruder@Ausars-MacBook-Pro.local>
2020-11-13 19:38:24 -0500

    frequency counter using an object

---
+++ Challanges_6/multPointFreq.js
@@ -0,0 +1,15 @@
+// Implement a function called, areThereDuplicates which accepts a
+// variable number of arguments and check whether there are any duplicates
+// among the arguments passed in. You can solve this using the frequency counter
+// pattern OR the multiple pointers pattern
+
+function areThereDuplicates(){
+    let collection = {};
+    for(let val in arguments){
+        collection[arguments[val]] = (collection[arguments[val]])
+    }
+    for(let key in collection){
+        if(collection[key] > 1) return true
+    }
+    return false;
+}
