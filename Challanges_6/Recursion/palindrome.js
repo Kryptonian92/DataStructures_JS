97f487408065bcadeab6a411f2bf6d164c5b84a2
Kryptonian92 <ausar_mcgruder@yahoo.com>
2020-12-30 23:40:46 -0500

    palindrome

---
+++ Challanges_6/Recursion/palindrome.js
@@ -0,0 +1,9 @@
+// palindrome recursively. Word spelled out is the same backwards
+// eg, tacocat
+
+function palindrome(str){
+    if(str.length === 1) return true;
+    if(str.length === 2) return str[0] ===str[1];
+    if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
+    return false;
+}
