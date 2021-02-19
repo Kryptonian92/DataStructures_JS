e1d67df037b5305c679938d33f2af4152647f6f8
Kryptonian92 <ausar_mcgruder@yahoo.com>
2021-02-19 09:30:04 -0500

    sorting algos..basic built in sort method

---
+++ Challanges_6/Bubble Sort/sort.js
@@ -0,0 +1,10 @@
+// Sorting using builting methods
+
+function compareByLen(str1, str2){
+    console.log(str2.length - str2.length)
+    return str2.length - str2.length;
+}
+[ "Lamborghini", "Ausar", "Data Structures", "Algorithms"]
+    .sort(compareByLen);
+
+// sorts arrays
