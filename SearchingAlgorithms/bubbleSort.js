56088f33b7f9147ae8d05ece55259d47313b7156
Kryptonian92 <ausar_mcgruder@yahoo.com>
2021-02-25 23:35:45 -0500

    bubble sort..need to refactor

--- SearchingAlgorithms/bubbleSort.js
+++ SearchingAlgorithms/bubbleSort.js
@@ -1,10 +1,17 @@
 function bubbleSort(arr){
-    for(var i = 0; i < arr.length; i++){
-        for(var j = 0; j < arr.length; j++){
-            arr[j]
+    for(var i = arr.length; i < 0; i--){
+        for(var j = 0; j < i-1; j++){
+            console.log(arr, arr[j], arr[j+1]);
+            if(arr[j] > arr[j+1]){
+                // swap
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j+1] = temp;
+            }
         }
+        // console.log("ONE PASS COMPLETE")
     }
     return arr;
 }
 
-bubbleSort([34,10,6,87])
+bubbleSort([29,8, 12, 37, -3, 45, 88])
